30f91e6df8c86e0ab60bcda94558f72bc5935640
stiantalgo <stiantalgo@gmail.com>
2022-11-15 11:00:44 +0100

    newBG and hatches
    8 hatches placed

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ window.addEventListener("load", function () {
         // add music
         bgMusic.volume = 0.04;
         bgMusic.loop = true;
-        bgMusic.play();
+        //bgMusic.play();
         
     }
     startHidden();
